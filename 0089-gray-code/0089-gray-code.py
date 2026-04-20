258de6e724a40d784ae4524dfb9c53d1a29d37ea
Rushikesh Patil <158311760+Rushi200-6@users.noreply.github.com>
2026-04-21 02:25:47 +0530

    Time: 7 ms (76.42%), Space: 20.8 MB (11.69%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.py
@@ -0,0 +1,9 @@
+class Solution:
+    def grayCode(self, n):
+        result = []
+        
+        for i in range(1 << n):
+            gray = i ^ (i >> 1)
+            result.append(gray)
+        
+        return result
