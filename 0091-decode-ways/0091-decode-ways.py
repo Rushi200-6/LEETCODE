ceb43a260ea6012d5aa26021f06420cde726eee4
Rushikesh Patil <158311760+Rushi200-6@users.noreply.github.com>
2026-08-01 18:54:57 +0530

    Time: 0 ms (100%), Space: 12.4 MB (28.86%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numDecodings(self, s):
+        if not s or s[0] == "0":
+            return 0
+
+        prev2 = 1   # dp[i-2]
+        prev1 = 1   # dp[i-1]
+
+        for i in range(1, len(s)):
+            curr = 0
+
+            # Single digit
+            if s[i] != "0":
+                curr += prev1
+
+            # Two digits
+            if "10" <= s[i-1:i+1] <= "26":
+                curr += prev2
+
+            prev2 = prev1
+            prev1 = curr
+
+        return prev1
