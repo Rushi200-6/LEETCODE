a8a9d4dc310a4516053adce66cba429edd7bc87c
Rushikesh Patil <158311760+Rushi200-6@users.noreply.github.com>
2026-08-13 18:23:33 +0530

    Time: 0 ms (100%), Space: 12.4 MB (57.52%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,22 +1,25 @@
 class Solution:
     def nextPermutation(self, nums):
-        n = len(nums)
-
-        
-        i = n - 2
-        while i >= 0 and nums[i] >= nums[i + 1]:
-            i -= 1
-
-        
-        if i >= 0:
-            j = n - 1
-            while nums[j] <= nums[i]:
-                j -= 1
-            nums[i], nums[j] = nums[j], nums[i]
-
-        
-        left, right = i + 1, n - 1
-        while left < right:
-            nums[left], nums[right] = nums[right], nums[left]
-            left += 1
-            right -= 1
+        n=len(nums)
+        pivot=-1
+        #find the element that smaller than jusnext number
+        for i in range(n-2,-1,-1):
+            if nums[i]<nums[i+1]:
+                pivot=i
+                break
+        #condition in leetcode
+        if pivot==-1:
+            nums.reverse()
+            return
+        #find the next greater number
+        for i in range(n-1,pivot,-1):
+            if nums[i]>nums[pivot]:
+                nums[i],nums[pivot]=nums[pivot],nums[i]
+                break
+        #reverse the next remaining part from pivot+1
+        i=pivot+1
+        j=n-1
+        while i<j:
+            nums[i],nums[j]=nums[j],nums[i]
+            i+=1
+            j-=1
