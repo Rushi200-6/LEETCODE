1a9b6eed1c1f14bd8492ff7b38cbaffe57e447d0
Rushikesh Patil <158311760+Rushi200-6@users.noreply.github.com>
2026-08-16 18:10:44 +0530

    Time: 1 ms (6.81%), Space: 12.9 MB (30.11%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,13 +1,14 @@
 class Solution(object):
     def searchInsert(self, nums, target):
-        left,right=0,len(nums)-1
-        while left<=right:
-            mid=(left+right)//2
+        l,r=0,len(nums)-1
+        while l<=r:
+            mid=l+(r-l)//2
             if nums[mid]==target:
                 return mid
             elif nums[mid]<target:
-                left=mid+1
+                l+=1
             else:
-                right=mid-1
-        return left
-        
+                r-=1
+        return l
+    
+#two pointers using binary search
