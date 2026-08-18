00997a1a767078e0045a40eb7b262f9a2c72edeb
Rushikesh Patil <158311760+Rushi200-6@users.noreply.github.com>
2026-08-18 13:51:23 +0530

    Time: 3 ms (9.51%), Space: 12.7 MB (73.57%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -1,30 +1,25 @@
 class Solution:
     def search(self, nums, target):
-        left, right = 0, len(nums) - 1
-        
-        while left <= right:
-            mid = (left + right) // 2
-            
-            if nums[mid] == target:
+        n=len(nums)
+        l,r=0,n-1
+        while l<=r:
+            mid=l+(r-l)//2
+            if nums[mid]==target:
                 return True
-            
-            
-            if nums[left] == nums[mid] == nums[right]:
-                left += 1
-                right -= 1
-            
-            
-            elif nums[left] <= nums[mid]:
-                if nums[left] <= target < nums[mid]:
-                    right = mid - 1
+            if nums[l]==nums[mid]==nums[r]:
+                l+=1
+                r-=1
+                continue
+            if nums[mid]<=nums[r]:
+                if nums[mid]<=target<=nums[r]:
+                    l=mid+1
                 else:
-                    left = mid + 1
-            
-            
+                    r=mid-1
             else:
-                if nums[mid] < target <= nums[right]:
-                    left = mid + 1
+                if nums[l]<=target<=nums[mid]:
+                    r=mid-1
                 else:
-                    right = mid - 1
-        
+                    l=mid+1
         return False
+                
+#similar to search in sorted array 1 but 1 extra additional condition required to handle the duplicates 
