947c18499a994bc46f513a87bdd10007baea6a0d
Rushikesh Patil <158311760+Rushi200-6@users.noreply.github.com>
2026-08-19 18:51:01 +0530

    Time: 0 ms (100%), Space: 13 MB (98.37%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,30 +1,45 @@
 class Solution(object):
-    def searchRange(self, nums, target):
-        def ffirst():
-            left,right=0,len(nums)-1
-            first=-1
 
-            while left<=right:
-                mid=(left+right)//2
-                if nums[mid]==target:
-                    first=mid
-                    right=mid-1
-                elif nums[mid]<target:
-                    left=mid+1
+    def searchRange(self, nums, target):
+
+        def lower_bound(nums, target):
+            n = len(nums)
+            lb = -1
+            l, r = 0, n - 1
+
+            while l <= r:
+                mid = l + (r - l) // 2
+
+                if nums[mid] >= target:
+                    lb = mid
+                    r = mid - 1
                 else:
-                    right=mid-1
-            return first
-        def llast():
-            left,right=0,len(nums)-1
-            last=-1
-            while left<=right:
-                mid=(left+right)//2
-                if  nums[mid]==target:
-                    last=mid
-                    left=mid+1
-                elif nums[mid]<target:
-                    left=mid+1
+                    l = mid + 1
+
+            return lb
+
+        def upper_bound(nums, target):
+            n = len(nums)
+            l, r = 0, n - 1
+            ub = n
+
+            while l <= r:
+                mid = l + (r - l) // 2
+
+                if nums[mid] > target:
+                    ub = mid
+                    r = mid - 1
                 else:
-                    right=mid-1
-            return last
-        return [ffirst(),llast()]
+                    l = mid + 1
+
+            return ub
+
+        lb = lower_bound(nums, target)
+        ub = upper_bound(nums, target)
+
+        if lb == -1 or nums[lb] != target:
+            return [-1, -1]
+
+        return [lb, ub - 1]
+
+        # use of upper bound and lower bound with binary search
