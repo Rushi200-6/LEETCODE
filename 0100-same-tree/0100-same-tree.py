4a65663f1252c6f2cff22fca3e68fd244dd3a242
Rushikesh Patil <158311760+Rushi200-6@users.noreply.github.com>
2026-08-25 21:26:45 +0530

    Time: 0 ms (100%), Space: 12.6 MB (23.15%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -1,5 +1,5 @@
 class Solution:
-    def isSameTree(self, p, q):
+    def isSameTree(self, p,q):
 
         if not p and not q:
             return True
@@ -12,5 +12,5 @@ class Solution:
         if p.val != q.val:
             return False
         
-  
+        # Recursive Step: Check if both left subtrees and right subtrees match
         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
