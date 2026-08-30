f5a845abc2efe1bad5d4baba6c1fc1d327b06911
Rushikesh Patil <158311760+Rushi200-6@users.noreply.github.com>
2026-08-30 12:53:40 +0530

    Time: 0 ms (100%), Space: 12.5 MB (28.24%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,15 +1,29 @@
 class Solution(object):
     def myPow(self, x, n):
-        if n==0:
-            return 1
-        if n<0:
-            x=1/x
-            n=-n
-        result=1
-        while n>0:
-            if n%2==1:
-                result*=x
-            x*=x
-            n=n//2
-        return result
+        # if n==0:
+        #     return 1
+        # if n<0:
+        #     x=1/x
+        #     n=-n
+        # result=1
+        # while n>0:
+        #     if n%2==1:
+        #         result*=x
+        #     x*=x
+        #     n=n//2
+        # return result
         
+        # by recursive method
+        def help(x,n):
+            if x==0:
+                return 0
+            if n==0:
+                return 1
+            res=help(x,n//2)
+            if n%2==0:
+                return res*res
+            else:
+                return res*res*x
+        if n<0:
+            return 1/help(x,-n)
+        return help(x,n)
