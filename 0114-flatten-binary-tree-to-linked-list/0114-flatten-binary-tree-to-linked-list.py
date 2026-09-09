9f4fc788045daef4e4b0389491708b9aeb25e2b2
Rushikesh Patil <158311760+Rushi200-6@users.noreply.github.com>
2026-09-09 19:22:16 +0530

    Time: 0 ms (100%), Space: 12.6 MB (78.81%) - LeetHub

---
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
@@ -0,0 +1,18 @@
+class Solution:
+    def flatten(self, root):
+        if not root:
+            return
+
+        self.flatten(root.left)
+        self.flatten(root.right)
+
+        right = root.right
+
+        root.right = root.left
+        root.left = None
+
+        curr = root
+        while curr.right:
+            curr = curr.right
+
+        curr.right = right
