ba55679a8191676dd1ea3ead2039534721f9ac2a
Rushikesh Patil <158311760+Rushi200-6@users.noreply.github.com>
2026-09-24 15:49:26 +0530

    Time: 145 ms (12.98%), Space: 19.5 MB (14.41%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -7,6 +7,6 @@ class Solution(object):
             min_price=min(min_price,prices[i])
             max_profit=max(max_profit,prices[i]-min_price)
         return max_profit
-        # on
+
        
         
